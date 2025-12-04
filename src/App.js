74e0a44feee4097b322d147b6b381027c039874c
dzsuhesz <grperge@gmail.com>
2025-12-04 20:18:51 +0100

    Refined texts and the button, plus added the image

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import { useState } from 'react';
 function App() {
   const [showImage, setShowImage] = useState(false);
   return (
-    <div>
-        <h1> I <code>dare</code> you click one this. </h1>
+    <div style={{ textAlign: "center"}}>
+        <h1> Don't you dare click on this. </h1>
         <button onClick={() => setShowImage(!showImage)}>
+          {showImage ? "You actually dared" : "I dare you"}
         </button>
         {showImage && (
           <div>
             <img src={logo} alt="the coolest cat" width="500"></img>
+            <h1>Damn you are cool</h1>
           </div>
         )}
-        <h1>And it's my first one! </h1>
+        
     </div>
   );
 }
