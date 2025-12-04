ab56c7958fc910e4cd02b1349443ae6550f30981
dzsuhesz <grperge@gmail.com>
2025-12-04 20:05:08 +0100

    Added cool button v0.1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
+import logo from './ok-cat.gif';
+import { useState } from 'react';
+
 function App() {
+  const [showImage, setShowImage] = useState(false);
   return (
     <div>
-      <header>
-        <p>
-          Just another <code>fancy</code> React page.
-        </p>
-      </header>
-      <h1>And it's my first one! </h1>
+        <h1> I <code>dare</code> you click one this. </h1>
+        <button onClick={() => setShowImage(!showImage)}>
+        </button>
+        {showImage && (
+          <div>
+            <img src={logo} alt="the coolest cat" width="500"></img>
+          </div>
+        )}
+        <h1>And it's my first one! </h1>
     </div>
   );
 }
-
 export default App;
