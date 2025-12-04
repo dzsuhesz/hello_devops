3e1bbbee8b56490bbe2b44e42b9b4e8530b7e9d5
dzsuhesz <grperge@gmail.com>
2025-12-04 19:31:19 +0100

    Added the coolest cat

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
+import logo from './ok-cat.gif';
+
 function App() {
   return (
     <div>
       <header>
-        <p>
-          Just another <code>fancy</code> React page.
-        </p>
+        <p> Just another <code>fancy</code> React page. </p>
+        <h1>And it's my first one! </h1>
+        <img src={logo} alt=""></img>
       </header>
-      <h1>And it's my first one! </h1>
     </div>
   );
 }
-
 export default App;
